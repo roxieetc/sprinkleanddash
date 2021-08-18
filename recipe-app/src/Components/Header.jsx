ec575444ae28a7b52c2e5f8331e1e8d410fa9969
roxieetc <roxieschnoor@yahoo.com>
2021-08-18 06:48:38 -0700

    Added styling to page

--- recipe-app/src/Components/Header.jsx
+++ recipe-app/src/Components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function Header() {
+  
+  
   return (
     <div className="Header">
-     <h2>This is the header!</h2>
+     <h2 className="appTitle">Sprinkle and Dash</h2>
+     <div className="headerRight"><input type='text' placeholder="Search for recipes..."></input>
+     <button>Search</button>
+     </div>
     </div>
   );
 }
--- recipe-app/src/Components/Nav.jsx
+++ recipe-app/src/Components/Nav.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 function Nav() {
   return (
     <div className="Nav">
-     <h3>This is the side nav</h3>
+     <h3 class="navTitle">Categories</h3>
+     <ul>
+       <li>Beef</li>
+       <li>Breakfast</li>
+       <li>Chicken</li>
+       <li>Dessert</li>
+       <li>Goat</li>
+       <li>Lamb</li>
+       <li>Miscellaneous</li>
+       <li>Pasta</li>
+       <li>Pork</li>
+       <li>Seafood</li>
+       <li>Side</li>
+       <li>Starter</li>
+       <li>Vegan</li>
+       <li>Vegetarian</li>
+     </ul>
     </div>
   );
 }

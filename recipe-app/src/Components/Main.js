9204ff4964b8ec75843e891e6e7814033b3b84f2
roxieetc <roxieschnoor@yahoo.com>
2021-08-19 09:21:15 -0700

    got random meal generator working

--- recipe-app/src/Components/Main.js
+++ recipe-app/src/Components/Main.js
@@ -3,29 +3,33 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
 const Main = () => {
-  const [recipe, setRecipe] = useState([])
+  const [randomMeal, setRandomMeal] = useState([])
+
+  const fetchFood = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data.meals)
+    setRandomMeal(data.meals)  
+  }
 
   useEffect(() => {
-    fetch(url)
-    .then((res) => res.json())
-    .then((json) => {
-      setRecipe(json);
-    })
-    .catch(console.error);
-  }, []);
+    fetchFood()
+  }, [])
 
   return (
-    <section className="container">
-      {recipe.map((food) => {
-        
-        return (
-          <div>
-            <h2>{food.strMeal}</h2>
-          </div>
+     <section className="meals">
+       {randomMeal.map((recipe) => {
+         const {idMeal, strMeal, strInstructions, strMealThumb} = recipe
 
-        )
-      })}
-    </section>
+         return (
+           <article key={idMeal}>
+             <h2>{strMeal}</h2>
+             <img src={strMealThumb} alt={strMeal} />
+             <p>{strInstructions}</p>
+           </article>
+         )
+       })}
+       </section> 
   );
 }
 

1050b42d604ebc76e80220cdb1e0d5c0fae02dab
roxieetc <roxieschnoor@yahoo.com>
2021-08-19 10:08:37 -0700

    added refresh onclick to title with pointer styling

--- recipe-app/src/Components/Header.jsx
+++ recipe-app/src/Components/Header.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 function Header() {
   
+  const refresh = () => {
+    window.location.reload(false);
+  }
   
   return (
     <div className="Header">
-     <h2 className="appTitle">Sprinkle and Dash</h2>
+     <h2 className="appTitle" onClick={refresh}>Sprinkle and Dash</h2>
      <div className="headerRight"><input type='text' placeholder="Search for recipes..."></input>
      <button>Search</button>
      </div>
